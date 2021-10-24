68abad864939bcc78de7afdbaf45a772615e80f8
togami2864 <tuabobo123@gmail.com>
2021-10-24 18:20:30 +0900

    chore: split code into files

---
+++ 50cc.h
@@ -0,0 +1,68 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// tokenize
+typedef enum { TK_RESERVED, TK_NUM, TK_EOF } TokenKind;
+
+typedef struct Token Token;
+struct Token {
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;
+  int len;
+};
+
+extern Token *token;
+extern char *user_input;
+
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+
+bool consume(char *op);
+void expect(char *op);
+int expect_number();
+bool at_eof();
+bool startswith(char *p, char *q);
+
+Token *new_token(TokenKind kind, Token *cur, char *str, int len);
+Token *tokenize();
+
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM,
+  ND_EQ,
+  ND_NE,
+  ND_LT,
+  ND_LE
+} NodeKind;
+
+typedef struct Node Node;
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
+Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
+Node *mul();
+Node *unary();
+Node *primary();
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+
+//gen
+void gen(Node *node);
+
