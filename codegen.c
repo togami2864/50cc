b6ee2c284620bbc66d62e7309bf6afed69b09b52
togami2864 <tuabobo123@gmail.com>
2021-11-01 14:27:44 +0900

    feat: impl boundory

--- codegen.c
+++ codegen.c
@@ -98,7 +98,18 @@ void gen(Node *node) {
       for (int i = argCount - 1; i >= 0; i--) {
         printf("  pop %s\n", argRegs[i]);
       }
+      printf("  mov rax, rsp\n");
+      printf("  and rax, 15\n");
+      printf("  jnz .L.call.%d\n", id);
+      printf("  mov rax, 0\n");
       printf("  call %s\n", name);
+      printf("  jmp .L.end.%d\n", id);
+      printf(".L.call.%d:\n", id);
+      printf("  sub rsp, 8\n");
+      printf("mov rax, 0\n");
+      printf("  call %s\n", name);
+      printf("  add rsp, 8\n");
+      printf(".L.end.%d:\n", id);
       return;
   }
 
